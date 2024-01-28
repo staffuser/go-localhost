927a2de495bb8d1d753bc0fb05879cefc02ec68d
staffuser <119116574+staffuser@users.noreply.github.com>
2024-01-29 01:14:02 +0300

    Create fail.go

---
+++ fail.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world !")
+}
+
+func main() {
+	http.HandleFunc("/", sayhello) // Устанавливаем роутер
+	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
+
+	// Если хотите использовать https, то вместо ListenAndServe используйте ListenAndServeTLS
+	// err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	
